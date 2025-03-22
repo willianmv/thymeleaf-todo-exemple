25b526b14894cde4dcba7ca51798bf9ff60883e3
willianmv <willianvmarques300@gmail.com>
2025-03-22 20:59:43 -0300

    Iniciando projeto

---
+++ src/test/java/com/example/thymeleaf_todo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
