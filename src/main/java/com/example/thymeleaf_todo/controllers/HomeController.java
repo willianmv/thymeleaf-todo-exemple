4b68536246924dc9308b4a1d82d3ea59b4666a40
willianmv <willianvmarques300@gmail.com>
2025-03-22 21:51:00 -0300

    Testa comunicação entre templates e controller

--- src/main/java/com/example/thymeleaf_todo/controllers/HomeController.java
+++ src/main/java/com/example/thymeleaf_todo/controllers/HomeController.java
@@ -2,15 +2,20 @@ package com.example.thymeleaf_todo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping()
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        var modelAndView = new ModelAndView("home");
+        modelAndView.addObject("nome", " Nome Testes");
+        var listaTestes = List.of("Teste1", "Teste2", "Teste3", "Teste4");
+        modelAndView.addObject("testes", listaTestes);
+        modelAndView.addObject("ehprogramador", true);
+        return modelAndView;
     }
-
 }
